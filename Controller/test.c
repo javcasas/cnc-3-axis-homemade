e5ce7b9f677252a0a3185b9cabba25cc8ebed480
Javier Casas <javcasas@gmail.com>
2017-09-24 22:49:52 -0500

    Prototyping traverse algorithm

--- Controller/test.c
+++ Controller/test.c
@@ -1,3 +1,4 @@
+#include "stdio.h"
 /*
   Blink
   Turns on an LED on for one second, then off for one second, repeatedly.
@@ -7,6 +8,7 @@
  
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
+/*
 int led = 13;
 
 int x1 = 6;
@@ -369,4 +371,137 @@ void loop() {
   } else {
   }
   delay(100);
+}
+*/
+
+struct Coord {
+  signed long x;
+  signed long y;
+  signed long z;
+};
+void print_coord(struct Coord c) {
+    printf("(%ld, %ld, %ld)\n", c.x, c.y, c.z);
+}
+
+struct Coord pos = {0,0,0};
+
+struct Coord source = {0,0,0};
+struct Coord target = {0,0,0};
+
+void move_x(int dir) { }
+void move_y(int dir) { }
+void move_z(int dir) { }
+
+struct Coord traverse_along_x() {
+  struct Coord trajectory = {
+    target.x - source.x,
+    target.y - source.y,
+    target.z - source.z
+  };
+  int next_x = sign(trajectory.x);
+  if (pos.x == target.x) {
+    next_x = 0;
+  }
+  double proportion = (double)(pos.x - source.x + next_x) / (double)trajectory.x;
+  //printf(" %f ", proportion);
+
+  struct Coord expected_position = {
+    source.x + trajectory.x * proportion,
+    source.y + trajectory.y * proportion,
+    source.z + trajectory.z * proportion
+  };
+  //printf("Expected ");
+  //print_coord(expected_position);
+  struct Coord error = {
+    pos.x - expected_position.x,
+    pos.y - expected_position.y,
+    pos.z - expected_position.z
+  };
+  move_x(sign(trajectory.x));
+  move_y(sign(error.y));
+  move_z(sign(error.z));
+  struct Coord movement = {
+    next_x,
+    sign(-error.y),
+    sign(-error.z)
+  };
+  return movement;
+}
+
+void traverse_along_y() {
+  
+}
+
+void traverse_along_z() {
+  
+}
+
+
+void traverse_to_coords() {
+  struct Coord trajectory = {
+    target.x - source.x,
+    target.y - source.y,
+    target.z - source.z
+  };
+  if ((abs(trajectory.x >= trajectory.y)) && (abs(trajectory.x >= trajectory.z))) {
+    traverse_along_x();
+  } else if (abs(trajectory.y >= trajectory.z)) {
+    traverse_along_y();
+  } else {
+    traverse_along_z();
+  }
+ 
+}
+
+int sign(signed long val) {
+  if (val > 0) {
+    return 1;
+  } else if (val < 0) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+
+void test_traverse_1() {
+    source.x = 5;
+    source.y = 2;
+    source.z = 3;
+
+    target.x = -10;
+    target.y = 6;
+    target.z = -1;
+
+    printf("Target: ");
+    print_coord(target);
+    pos = source;
+    int steps = 0;
+    struct Coord increment;
+    printf("Expected steps: %ld\n", target.x-pos.x);
+    while ((pos.x != target.x) || (pos.y != target.y) || (pos.z != target.z))
+    {
+        //printf("Position: ");
+        //print_coord(pos);
+        increment = traverse_along_x();
+        //printf("Increment: ");
+        //print_coord(increment);
+        pos.x += increment.x;
+        pos.y += increment.y;
+        pos.z += increment.z;
+        steps += 1;
+        
+    }
+    printf("Done in %d steps.", steps);
+}
+
+
+int main() {
+    print_coord(pos);
+    pos.x = 3;
+    source.x = 4;
+    target.x = -4;
+    print_coord(traverse_along_x());
+    test_traverse_1();
+    return 0;
 }
