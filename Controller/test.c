6d1c0d84796741a31feaa3bcb02c5082d30dedbc
Javier Casas <javcasas@gmail.com>
2017-10-01 22:22:24 -0500

    Extracting tests properly

--- Controller/test.c
+++ Controller/test.c
@@ -1,470 +1,18 @@
 #include "stdio.h"
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-/*
-int led = 13;
+#include "traverse.c"
 
-int x1 = 6;
-int x2 = 7;
-int xmin = 8;
-int xmax = 9;
-int xstep1 = 10;
-int xstep2 = 11;
-
-int y1 = 4;
-int y2 = 5;
-int ymin = 8;
-int ymax = 9;
-int ystep1 = 10;
-int ystep2 = 11;
-
-int z1 = 6;
-int z2 = 7;
-int zmin = 8;
-int zmax = 9;
-int zstep1 = 10;
-int zstep2 = 11;
-
-void setup_motors() {
-  halt();
-  pinMode(x1, OUTPUT);
-  pinMode(x2, OUTPUT);
-
-  pinMode(y1, OUTPUT);
-  pinMode(y2, OUTPUT);
-
-  pinMode(z1, OUTPUT);
-  pinMode(z2, OUTPUT);
-  halt();
-}
-
-void setup_sensors() {
-  pinMode(xmin, INPUT_PULLUP);
-  pinMode(xmax, INPUT_PULLUP);
-  pinMode(xstep1, INPUT_PULLUP);
-  pinMode(xstep2, INPUT_PULLUP);
-
-  pinMode(ymin, INPUT_PULLUP);
-  pinMode(ymax, INPUT_PULLUP);
-  pinMode(ystep1, INPUT_PULLUP);
-  pinMode(ystep2, INPUT_PULLUP);
-
-  pinMode(zmin, INPUT_PULLUP);
-  pinMode(zmax, INPUT_PULLUP);
-  pinMode(zstep1, INPUT_PULLUP);
-  pinMode(zstep2, INPUT_PULLUP);
-}
-
-
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);
-  setup_sensors();
-  setup_motors();
-  preload_servos();
-}
-
-void move_axis(int p1, int p2, int dir) {
-  if (dir > 0) {
-    digitalWrite(p1, HIGH);
-    digitalWrite(p2, LOW);
-  } else if (dir < 0) {
-    digitalWrite(p1, LOW);
-    digitalWrite(p2, HIGH);
-  } else {
-    digitalWrite(p1, LOW);
-    digitalWrite(p2, LOW);
-  }
-}
-
-int sign(signed long val) {
-  if (val > 0) {
-    return 1;
-  } else if (val < 0) {
-    return -1;
-  } else {
-    return 0;
-  }
-}
-
-void move_x(int dir) {
-  move_axis(x1, x2, dir);
-}
-
-void move_y(int dir) {
-  move_axis(y1, y2, dir);
-}
-
-void move_z(int dir) {
-  move_axis(z1, z2, dir);
-}
-
-void halt() {
-  move_x(0);
-  move_y(0);
-  move_z(0);
-}
-
-void update_servos() {
-  update_x();
-  update_y();
-  update_z();
-}
-
-struct Coord {
-  signed long x;
-  signed long y;
-  signed long z;
-};
-
-struct Coord pos = {0,0,0};
-
-struct Coord source = {0,0,0};
-struct Coord target = {0,0,0};
-
-//signed long x_pos = 0;
-//signed long y_pos = 0;
-//signed long z_pos = 0;
-
-//signed long target_x_pos = 0;
-//signed long target_y_pos = 0;
-//signed long target_z_pos = 0;
-
-int prev_x1 = 0;
-int prev_x2 = 0;
-int prev_y1 = 0;
-int prev_y2 = 0;
-int prev_z1 = 0;
-int prev_z2 = 0;
-
-
-void preload_servos () {
-  prev_x1 = digitalRead(xstep1);
-  prev_x2 = digitalRead(xstep2);
-  prev_y1 = digitalRead(ystep1);
-  prev_y2 = digitalRead(ystep2);
-  prev_z1 = digitalRead(zstep1);
-  prev_z2 = digitalRead(zstep2);
-}
-
-int calc_direction (int p1, int p2, int c1, int c2) {
-  if ((p1 == 0) && (p2 == 0) && (c1 == 0) && (c2 == 1)) {
-    return 1;
-  } else if ((p1 == 0) && (p2 == 1) && (c1 == 1) && (c2 == 1)) {
-    return 1;
-  } else if ((p1 == 1) && (p2 == 1) && (c1 == 1) && (c2 == 0)) {
-    return 1;
-  } else if ((p1 == 1) && (p2 == 0) && (c1 == 0) && (c2 == 0)) {
-    return 1;
-  } else if ((p1 == 1) && (p2 == 0) && (c1 == 1) && (c2 == 1)) {
-    return -1;
-  } else if ((p1 == 1) && (p2 == 1) && (c1 == 0) && (c2 == 1)) {
-    return -1;
-  } else if ((p1 == 0) && (p2 == 1) && (c1 == 0) && (c2 == 0)) {
-    return -1;
-  } else if ((p1 == 0) && (p2 == 0) && (c1 == 1) && (c2 == 0)) {
-    return -1;
-  } else {
-    return 0;
-  }
-}
-
-void update_x() {
-  int dir = calc_direction(prev_x1, prev_x2, digitalRead(xstep1), digitalRead(xstep2));
-  pos.x = pos.x + dir;
-  prev_x1 = digitalRead(xstep1);
-  prev_x2 = digitalRead(xstep2);
-}
-
-void update_y() {
-  int dir = calc_direction(prev_y1, prev_y2, digitalRead(ystep1), digitalRead(ystep2));
-  pos.y = pos.y + dir;
-  prev_y1 = digitalRead(ystep1);
-  prev_y2 = digitalRead(ystep2);
-}
-
-void update_z() {
-  int dir = calc_direction(prev_z1, prev_z2, digitalRead(zstep1), digitalRead(zstep2));
-  pos.z = pos.z + dir;
-  prev_z1 = digitalRead(zstep1);
-  prev_z2 = digitalRead(zstep2);
-}
-
-#define MODE_GO_TO_HOME_X 10
-#define MODE_GO_TO_HOME_X_RETURN 11
-#define MODE_GO_TO_END_X 12
-
-#define MODE_GO_TO_HOME_Y 20
-#define MODE_GO_TO_HOME_Y_RETURN 21
-#define MODE_GO_TO_END_Y 22
-
-#define MODE_GO_TO_HOME_Z 30
-#define MODE_GO_TO_HOME_Z_RETURN 31
-#define MODE_GO_TO_END_Z 32
-
-#define MODE_STOP 50
-#define MODE_GO_TO_COORDS 51
-
-int mode = MODE_GO_TO_HOME_Y;
-int counter = 0;
-int blink_pattern = 0b1111111111111111;
-
-void go_to_home_x() {
-  // Find the X start switch
-  if (digitalRead(xmin)) {
-     move_x(1);
-   } else {
-     move_x(0);
-     mode = MODE_GO_TO_HOME_X_RETURN;
-   }
-}
-
-void go_to_home_x_return() {
-  if (digitalRead(xmin)) {
-     mode = MODE_STOP;
-     move_x(0);
-   } else {
-     move_x(-1);
-   }
-}
-
-void go_to_home_y() {
-  // Find the Y start switch
-  if (digitalRead(ymin)) {
-     move_y(1);
-   } else {
-     move_y(0);
-     mode = MODE_GO_TO_HOME_Y_RETURN;
-   }
-}
-
-void go_to_home_y_return() {
-  if (digitalRead(ymin)) {
-     mode = MODE_STOP;
-     move_y(0);
-   } else {
-     move_y(-1);
-   }
-}
-
-void go_to_home_z() {
-  // Find the Z start switch
-  if (digitalRead(zmin)) {
-     move_z(1);
-   } else {
-     move_z(0);
-     mode = MODE_GO_TO_HOME_Z_RETURN;
-   }
-}
-
-void go_to_home_z_return() {
-  if (digitalRead(zmin)) {
-     mode = MODE_STOP;
-     move_z(0);
-   } else {
-     move_z(-1);
-   }
-}
-
-void stop_() {
-  halt();
-}
-
-void traverse_along_x() {
-  struct Coord trajectory = {
-    target.x - source.x,
-    target.y - source.y,
-    target.z - source.z
-  };
-  signed long position_along_trajectory = pos.x - trajectory.x;
-  double proportion = position_along_trajectory / trajectory.x;
-
-  struct Coord expected_position = {
-    source.x + position_along_trajectory,
-    source.y + trajectory.y * proportion,
-    source.z + trajectory.z * proportion
-  };
-  struct Coord error = {
-    pos.x - expected_position.x,
-    pos.y - expected_position.y,
-    pos.z - expected_position.z
-  };
-  move_x(sign(trajectory.x));
-  move_y(sign(error.y));
-  move_z(sign(error.z));
-}
-
-void traverse_along_y() {
-  
-}
-
-void traverse_along_z() {
-  
-}
-
-
-void traverse_to_coords() {
-  struct Coord trajectory = {
-    target.x - source.x,
-    target.y - source.y,
-    target.z - source.z
-  };
-  if ((abs(trajectory.x >= trajectory.y)) && (abs(trajectory.x >= trajectory.z))) {
-    traverse_along_x();
-  } else if (abs(trajectory.y >= trajectory.z)) {
-    traverse_along_y();
-  } else {
-    traverse_along_z();
-  }
- 
-}
-
-void blink_pattern_() {
-  int should_blink = 0;
-  counter = counter + 1;
-  if (counter > 16) {
-    counter = 0;
-  }
-  should_blink = (1 << counter) == 0;
-  digitalWrite(led, should_blink);
-}
-
-// the loop routine runs over and over again forever:
-void loop() {
-  blink_pattern_();
-  update_x();
-  update_y();
-  update_z();
-
-  if (mode == MODE_GO_TO_HOME_X) {
-    blink_pattern = 0b1111000011110000;
-    go_to_home_x();
-  } else if (mode == MODE_GO_TO_HOME_X_RETURN) {
-    blink_pattern = 0b1100110011000011;
-    go_to_home_x_return();
-  } else if (mode == MODE_GO_TO_HOME_Y) {
-    blink_pattern = 0b1111000011110000;
-    go_to_home_y();
-  } else if (mode == MODE_GO_TO_HOME_Y_RETURN) {
-    blink_pattern = 0b1100110011001100;
-    go_to_home_y_return();
-  } else if (mode == MODE_GO_TO_HOME_Z) {
-    blink_pattern = 0b1111000011110000;
-    go_to_home_z();
-  } else if (mode == MODE_GO_TO_HOME_Z_RETURN) {
-    blink_pattern = 0b1100110011001100;
-    go_to_home_z_return();
-  } else if (mode == MODE_STOP) {
-    blink_pattern = 0b1111111111111111;
-    stop_();
-  } else if (mode == MODE_GO_TO_COORDS) {
-    blink_pattern = 0b1111111100000000;
-    traverse_to_coords();
-  } else {
-  }
-  delay(100);
-}
-*/
-
-struct Coord {
-  signed long x;
-  signed long y;
-  signed long z;
-};
 void print_coord(struct Coord c) {
-    printf("(%ld, %ld, %ld)\n", c.x, c.y, c.z);
+    printf("(%ld, %ld, %ld)", c.x, c.y, c.z);
 }
 
-struct Coord pos = {0,0,0};
-
-struct Coord source = {0,0,0};
-struct Coord target = {0,0,0};
-
-void move_x(int dir) { }
-void move_y(int dir) { }
-void move_z(int dir) { }
-
-struct Coord traverse_along_x() {
-  struct Coord trajectory = {
-    target.x - source.x,
-    target.y - source.y,
-    target.z - source.z
-  };
-  int next_x = sign(trajectory.x);
-  if (pos.x == target.x) {
-    next_x = 0;
-  }
-  double proportion = (double)(pos.x - source.x + next_x) / (double)trajectory.x;
-  //printf(" %f ", proportion);
-
-  struct Coord expected_position = {
-    source.x + trajectory.x * proportion,
-    source.y + trajectory.y * proportion,
-    source.z + trajectory.z * proportion
-  };
-  //printf("Expected ");
-  //print_coord(expected_position);
-  struct Coord error = {
-    pos.x - expected_position.x,
-    pos.y - expected_position.y,
-    pos.z - expected_position.z
-  };
-  move_x(sign(trajectory.x));
-  move_y(sign(error.y));
-  move_z(sign(error.z));
-  struct Coord movement = {
-    next_x,
-    sign(-error.y),
-    sign(-error.z)
-  };
-  return movement;
+int run_test(char * name, int test()) {
+    printf("\n>>>> Executing %s\n", name);
+    int result = test();
+    printf("\n>>>> Result %s: %s\n", name, result ? "ERROR" : "Success");
+    return result;
 }
 
-void traverse_along_y() {
-  
-}
-
-void traverse_along_z() {
-  
-}
-
-
-void traverse_to_coords() {
-  struct Coord trajectory = {
-    target.x - source.x,
-    target.y - source.y,
-    target.z - source.z
-  };
-  if ((abs(trajectory.x >= trajectory.y)) && (abs(trajectory.x >= trajectory.z))) {
-    traverse_along_x();
-  } else if (abs(trajectory.y >= trajectory.z)) {
-    traverse_along_y();
-  } else {
-    traverse_along_z();
-  }
- 
-}
-
-int sign(signed long val) {
-  if (val > 0) {
-    return 1;
-  } else if (val < 0) {
-    return -1;
-  } else {
-    return 0;
-  }
-}
-
-
-void test_traverse_1() {
+int test_traverse_1() {
     source.x = 5;
     source.y = 2;
     source.z = 3;
@@ -473,12 +21,11 @@ void test_traverse_1() {
     target.y = 6;
     target.z = -1;
 
-    printf("Target: ");
-    print_coord(target);
     pos = source;
+    int expected_steps = abs(target.x - pos.x);
+    printf("Expected steps: %d\n", expected_steps);
     int steps = 0;
     struct Coord increment;
-    printf("Expected steps: %ld\n", target.x-pos.x);
     while ((pos.x != target.x) || (pos.y != target.y) || (pos.z != target.z))
     {
         //printf("Position: ");
@@ -493,15 +40,43 @@ void test_traverse_1() {
         
     }
     printf("Done in %d steps.", steps);
+    return expected_steps != steps;
+}
+
+int test_traverse_2() {
+    source.x = -5;
+    source.y = 2;
+    source.z = 3;
+
+    target.x = 10;
+    target.y = 6;
+    target.z = -1;
+
+    pos = source;
+    int expected_steps = abs(target.x - pos.x);
+    printf("Expected steps: %d\n", expected_steps);
+    int steps = 0;
+    struct Coord increment;
+    while ((pos.x != target.x) || (pos.y != target.y) || (pos.z != target.z))
+    {
+        //printf("Position: ");
+        //print_coord(pos);
+        increment = traverse_along_x();
+        //printf("Increment: ");
+        //print_coord(increment);
+        pos.x += increment.x;
+        pos.y += increment.y;
+        pos.z += increment.z;
+        steps += 1;
+        
+    }
+    printf("Done in %d steps.", steps);
+    return expected_steps != steps;
 }
 
 
 int main() {
-    print_coord(pos);
-    pos.x = 3;
-    source.x = 4;
-    target.x = -4;
-    print_coord(traverse_along_x());
-    test_traverse_1();
+    run_test("test_traverse_1", test_traverse_1);
+    run_test("test_traverse_2", test_traverse_2);
     return 0;
 }
--- Controller/traverse.c
+++ Controller/traverse.c
@@ -1,87 +1,4 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 13;
-
-int x1 = 6;
-int x2 = 7;
-int xmin = 8;
-int xmax = 9;
-int xstep1 = 10;
-int xstep2 = 11;
-
-int y1 = 4;
-int y2 = 5;
-int ymin = 8;
-int ymax = 9;
-int ystep1 = 10;
-int ystep2 = 11;
-
-int z1 = 6;
-int z2 = 7;
-int zmin = 8;
-int zmax = 9;
-int zstep1 = 10;
-int zstep2 = 11;
-
-void setup_motors() {
-  halt();
-  pinMode(x1, OUTPUT);
-  pinMode(x2, OUTPUT);
-
-  pinMode(y1, OUTPUT);
-  pinMode(y2, OUTPUT);
-
-  pinMode(z1, OUTPUT);
-  pinMode(z2, OUTPUT);
-  halt();
-}
-
-void setup_sensors() {
-  pinMode(xmin, INPUT_PULLUP);
-  pinMode(xmax, INPUT_PULLUP);
-  pinMode(xstep1, INPUT_PULLUP);
-  pinMode(xstep2, INPUT_PULLUP);
-
-  pinMode(ymin, INPUT_PULLUP);
-  pinMode(ymax, INPUT_PULLUP);
-  pinMode(ystep1, INPUT_PULLUP);
-  pinMode(ystep2, INPUT_PULLUP);
-
-  pinMode(zmin, INPUT_PULLUP);
-  pinMode(zmax, INPUT_PULLUP);
-  pinMode(zstep1, INPUT_PULLUP);
-  pinMode(zstep2, INPUT_PULLUP);
-}
-
-
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);
-  setup_sensors();
-  setup_motors();
-  preload_servos();
-}
-
-void move_axis(int p1, int p2, int dir) {
-  if (dir > 0) {
-    digitalWrite(p1, HIGH);
-    digitalWrite(p2, LOW);
-  } else if (dir < 0) {
-    digitalWrite(p1, LOW);
-    digitalWrite(p2, HIGH);
-  } else {
-    digitalWrite(p1, LOW);
-    digitalWrite(p2, LOW);
-  }
-}
+#include "stdlib.h"
 
 int sign(signed long val) {
   if (val > 0) {
@@ -91,30 +8,6 @@ int sign(signed long val) {
   } else {
     return 0;
   }
-}
-
-void move_x(int dir) {
-  move_axis(x1, x2, dir);
-}
-
-void move_y(int dir) {
-  move_axis(y1, y2, dir);
-}
-
-void move_z(int dir) {
-  move_axis(z1, z2, dir);
-}
-
-void halt() {
-  move_x(0);
-  move_y(0);
-  move_z(0);
-}
-
-void update_servos() {
-  update_x();
-  update_y();
-  update_z();
 }
 
 struct Coord {
@@ -128,168 +21,30 @@ struct Coord pos = {0,0,0};
 struct Coord source = {0,0,0};
 struct Coord target = {0,0,0};
 
-//signed long x_pos = 0;
-//signed long y_pos = 0;
-//signed long z_pos = 0;
+void move_x(int dir) { }
+void move_y(int dir) { }
+void move_z(int dir) { }
 
-//signed long target_x_pos = 0;
-//signed long target_y_pos = 0;
-//signed long target_z_pos = 0;
-
-int prev_x1 = 0;
-int prev_x2 = 0;
-int prev_y1 = 0;
-int prev_y2 = 0;
-int prev_z1 = 0;
-int prev_z2 = 0;
-
-
-void preload_servos () {
-  prev_x1 = digitalRead(xstep1);
-  prev_x2 = digitalRead(xstep2);
-  prev_y1 = digitalRead(ystep1);
-  prev_y2 = digitalRead(ystep2);
-  prev_z1 = digitalRead(zstep1);
-  prev_z2 = digitalRead(zstep2);
-}
-
-int calc_direction (int p1, int p2, int c1, int c2) {
-  if ((p1 == 0) && (p2 == 0) && (c1 == 0) && (c2 == 1)) {
-    return 1;
-  } else if ((p1 == 0) && (p2 == 1) && (c1 == 1) && (c2 == 1)) {
-    return 1;
-  } else if ((p1 == 1) && (p2 == 1) && (c1 == 1) && (c2 == 0)) {
-    return 1;
-  } else if ((p1 == 1) && (p2 == 0) && (c1 == 0) && (c2 == 0)) {
-    return 1;
-  } else if ((p1 == 1) && (p2 == 0) && (c1 == 1) && (c2 == 1)) {
-    return -1;
-  } else if ((p1 == 1) && (p2 == 1) && (c1 == 0) && (c2 == 1)) {
-    return -1;
-  } else if ((p1 == 0) && (p2 == 1) && (c1 == 0) && (c2 == 0)) {
-    return -1;
-  } else if ((p1 == 0) && (p2 == 0) && (c1 == 1) && (c2 == 0)) {
-    return -1;
-  } else {
-    return 0;
-  }
-}
-
-void update_x() {
-  int dir = calc_direction(prev_x1, prev_x2, digitalRead(xstep1), digitalRead(xstep2));
-  pos.x = pos.x + dir;
-  prev_x1 = digitalRead(xstep1);
-  prev_x2 = digitalRead(xstep2);
-}
-
-void update_y() {
-  int dir = calc_direction(prev_y1, prev_y2, digitalRead(ystep1), digitalRead(ystep2));
-  pos.y = pos.y + dir;
-  prev_y1 = digitalRead(ystep1);
-  prev_y2 = digitalRead(ystep2);
-}
-
-void update_z() {
-  int dir = calc_direction(prev_z1, prev_z2, digitalRead(zstep1), digitalRead(zstep2));
-  pos.z = pos.z + dir;
-  prev_z1 = digitalRead(zstep1);
-  prev_z2 = digitalRead(zstep2);
-}
-
-#define MODE_GO_TO_HOME_X 10
-#define MODE_GO_TO_HOME_X_RETURN 11
-#define MODE_GO_TO_END_X 12
-
-#define MODE_GO_TO_HOME_Y 20
-#define MODE_GO_TO_HOME_Y_RETURN 21
-#define MODE_GO_TO_END_Y 22
-
-#define MODE_GO_TO_HOME_Z 30
-#define MODE_GO_TO_HOME_Z_RETURN 31
-#define MODE_GO_TO_END_Z 32
-
-#define MODE_STOP 50
-#define MODE_GO_TO_COORDS 51
-
-int mode = MODE_GO_TO_HOME_Y;
-int counter = 0;
-int blink_pattern = 0b1111111111111111;
-
-void go_to_home_x() {
-  // Find the X start switch
-  if (digitalRead(xmin)) {
-     move_x(1);
-   } else {
-     move_x(0);
-     mode = MODE_GO_TO_HOME_X_RETURN;
-   }
-}
-
-void go_to_home_x_return() {
-  if (digitalRead(xmin)) {
-     mode = MODE_STOP;
-     move_x(0);
-   } else {
-     move_x(-1);
-   }
-}
-
-void go_to_home_y() {
-  // Find the Y start switch
-  if (digitalRead(ymin)) {
-     move_y(1);
-   } else {
-     move_y(0);
-     mode = MODE_GO_TO_HOME_Y_RETURN;
-   }
-}
-
-void go_to_home_y_return() {
-  if (digitalRead(ymin)) {
-     mode = MODE_STOP;
-     move_y(0);
-   } else {
-     move_y(-1);
-   }
-}
-
-void go_to_home_z() {
-  // Find the Z start switch
-  if (digitalRead(zmin)) {
-     move_z(1);
-   } else {
-     move_z(0);
-     mode = MODE_GO_TO_HOME_Z_RETURN;
-   }
-}
-
-void go_to_home_z_return() {
-  if (digitalRead(zmin)) {
-     mode = MODE_STOP;
-     move_z(0);
-   } else {
-     move_z(-1);
-   }
-}
-
-void stop_() {
-  halt();
-}
-
-void traverse_along_x() {
+struct Coord traverse_along_x() {
   struct Coord trajectory = {
     target.x - source.x,
     target.y - source.y,
     target.z - source.z
   };
-  signed long position_along_trajectory = pos.x - trajectory.x;
-  double proportion = position_along_trajectory / trajectory.x;
+  int next_x = sign(trajectory.x);
+  if (pos.x == target.x) {
+    next_x = 0;
+  }
+  double proportion = (double)(pos.x - source.x + next_x) / (double)trajectory.x;
+  //printf(" %f ", proportion);
 
   struct Coord expected_position = {
-    source.x + position_along_trajectory,
+    source.x + trajectory.x * proportion,
     source.y + trajectory.y * proportion,
     source.z + trajectory.z * proportion
   };
+  //printf("Expected ");
+  //print_coord(expected_position);
   struct Coord error = {
     pos.x - expected_position.x,
     pos.y - expected_position.y,
@@ -298,6 +53,12 @@ void traverse_along_x() {
   move_x(sign(trajectory.x));
   move_y(sign(error.y));
   move_z(sign(error.z));
+  struct Coord movement = {
+    next_x,
+    sign(-error.y),
+    sign(-error.z)
+  };
+  return movement;
 }
 
 void traverse_along_y() {
@@ -325,48 +86,3 @@ void traverse_to_coords() {
  
 }
 
-void blink_pattern_() {
-  int should_blink = 0;
-  counter = counter + 1;
-  if (counter > 16) {
-    counter = 0;
-  }
-  should_blink = (1 << counter) == 0;
-  digitalWrite(led, should_blink);
-}
-
-// the loop routine runs over and over again forever:
-void loop() {
-  blink_pattern_();
-  update_x();
-  update_y();
-  update_z();
-
-  if (mode == MODE_GO_TO_HOME_X) {
-    blink_pattern = 0b1111000011110000;
-    go_to_home_x();
-  } else if (mode == MODE_GO_TO_HOME_X_RETURN) {
-    blink_pattern = 0b1100110011000011;
-    go_to_home_x_return();
-  } else if (mode == MODE_GO_TO_HOME_Y) {
-    blink_pattern = 0b1111000011110000;
-    go_to_home_y();
-  } else if (mode == MODE_GO_TO_HOME_Y_RETURN) {
-    blink_pattern = 0b1100110011001100;
-    go_to_home_y_return();
-  } else if (mode == MODE_GO_TO_HOME_Z) {
-    blink_pattern = 0b1111000011110000;
-    go_to_home_z();
-  } else if (mode == MODE_GO_TO_HOME_Z_RETURN) {
-    blink_pattern = 0b1100110011001100;
-    go_to_home_z_return();
-  } else if (mode == MODE_STOP) {
-    blink_pattern = 0b1111111111111111;
-    stop_();
-  } else if (mode == MODE_GO_TO_COORDS) {
-    blink_pattern = 0b1111111100000000;
-    traverse_to_coords();
-  } else {
-  }
-  delay(100);
-}
